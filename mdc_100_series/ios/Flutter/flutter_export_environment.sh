fabf306b9fb3e61916f6da209516b70affdae041
Minji Kang <aldalddl2007@gmail.com>
2019-09-26 23:12:19 +0900

    Initial commit

---
+++ mdc_100_series/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aldalddl/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aldalddl/Downloads/material-components-flutter-codelabs-101-starter/mdc_100_series"
+export "FLUTTER_TARGET=/Users/aldalddl/Downloads/material-components-flutter-codelabs-101-starter/mdc_100_series/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/aldalddl/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
